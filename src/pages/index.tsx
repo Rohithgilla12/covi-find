65fb90e917a2ba5f633619a8ee99c7b048bc2f84
Rohithgilla12 <gillarohith1@gmail.com>
2021-04-27 20:45:40 +0530

    Moving to leaflet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
-import { MapComponent } from "../components/Map";
+// import MapComponent from "../components/Map";
 import { Container } from "../components/Container";
 import { CTA } from "../components/CTA";
 import { useState } from "react";
+import dynamic from "next/dynamic";
 import { Options, SelectedButtonContext } from "../context/ButtonSelection";
+
+const MapComponentNoSSR = dynamic<any>(() => import("../components/Map"), {
+  ssr: false,
+});
 
 const Index = () => {
   const [selctedButton, setSelection] = useState(Options.Hospitals);
@@ -11,7 +16,7 @@ const Index = () => {
     <Container height="100vh">
       <SelectedButtonContext.Provider value={{ selctedButton, setSelection }}>
         <Container mt={"10vh"}>
-          <MapComponent />
+          <MapComponentNoSSR />
         </Container>
         <CTA />
       </SelectedButtonContext.Provider>
