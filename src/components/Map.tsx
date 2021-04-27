491fbb0f45ad8a5a15b652851dec99bd4dd7eddc
Rohithgilla12 <gillarohith1@gmail.com>
2021-04-27 19:21:12 +0530

    Basic things working for delhi,

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,7 +5,7 @@ import {
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-import axios from "axios";
+import useSWR from "swr";
 // import { useSelectedButton } from "../context/ButtonSelection";
 
 const containerStyle = {
@@ -15,54 +15,90 @@ const containerStyle = {
 
 export const MapComponent = () => {
   const [current, setCurrent] = useState({ lat: 18.4264677, lng: 79.1339878 });
-  const [response, setResponse] = useState({});
+  // const [response, setResponse] = useState({});
   const [selectedCenter, setSelectedCenter] = useState<any | null>(null);
+  var lats: Array<number> = [];
+  var longs: Array<number> = [];
+
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const createNavLink = (lat: number, long: number) => {
+    if (
+      navigator.platform.indexOf("iPhone") != -1 ||
+      navigator.platform.indexOf("iPod") != -1 ||
+      navigator.platform.indexOf("iPad") != -1
+    )
+      return `maps://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=${lat},${long}`;
+    else
+      return `https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=${lat},${long}`;
+  };
+
+  const getCenterPoint = () => {
+    lats = [];
+    longs = [];
+    if (mapData) {
+      Object.entries(mapData).forEach((key: any, _value: any) => {
+        const position = key[1];
+        console.log(position);
+        lats.push(position["Lat"]);
+        longs.push(position["Long"]);
+      });
+      const sumLat = lats.reduce((a: number, b: number) => a + b, 0);
+      const sumLong = longs.reduce((a: number, b: number) => a + b, 0);
+      setCurrent({
+        lat: sumLat / lats.length,
+        lng: sumLong / longs.length,
+      });
+    }
+  };
+
+  const { data: mapData, error: _mapError } = useSWR(
+    "https://ach4l.pythonanywhere.com/covifind/delhi",
+    fetcher
+  );
   // const { selctedButton } = useSelectedButton();
 
   useEffect(() => {
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(async function (position) {
-        setCurrent({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-        const res = await axios.post(
-          "https://ach4l.pythonanywhere.com/findhosp",
-          {
-            lat: position.coords.latitude,
-            long: position.coords.longitude,
-          }
-        );
-        setResponse(res.data);
-      });
+      // navigator.geolocation.getCurrentPosition(async function (position) {
+      // setCurrent({
+      //   lat: position.coords.latitude,
+      //   lng: position.coords.longitude,
+      // });
+      // });
     }
   }, []);
-  console.log(response);
+
+  useEffect(() => {
+    getCenterPoint();
+  }, [mapData]);
+
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_GOOGLE_MAPS_API_KEY}>
-      <GoogleMap mapContainerStyle={containerStyle} center={current} zoom={4}>
-        <Marker position={current} />
-        {Object.entries(response).map((key: any, value: any) => {
-          const position = key[1];
-          console.log(position);
-          return (
-            <Marker
-              key={value}
-              position={{
-                lat: position["Lat"],
-                lng: position["Long"],
-              }}
-              onClick={() => {
-                setSelectedCenter({
+      <GoogleMap mapContainerStyle={containerStyle} center={current} zoom={12}>
+        {/* <Marker position={current} /> */}
+        {mapData &&
+          Object.entries(mapData).map((key: any, value: any) => {
+            const position = key[1];
+            return (
+              <Marker
+                key={value}
+                position={{
                   lat: position["Lat"],
                   lng: position["Long"],
-                  id: value,
-                  ...position,
-                });
-              }}
-            ></Marker>
-          );
-        })}
+                }}
+                icon="/icu-bed.png"
+                onClick={() => {
+                  setSelectedCenter({
+                    lat: position["Lat"],
+                    lng: position["Long"],
+                    id: value,
+                    ...position,
+                  });
+                }}
+              ></Marker>
+            );
+          })}
         {selectedCenter && (
           <InfoWindow
             onCloseClick={() => {
@@ -77,15 +113,18 @@ export const MapComponent = () => {
               <div>{selectedCenter["Hosp_name"]}</div>
               <div>{selectedCenter["Govt"]}</div>
               <div>Lasted updated at {selectedCenter["Last_updated"]}</div>
-              <div>Total ICU Beds {selectedCenter["Total_ICU_beds"]}</div>
-              <div>Total Normal Beds {selectedCenter["Total_normal_beds"]}</div>
-              <div>Vacant Beds {selectedCenter["Vacant_ICU_beds"]}</div>
+              <div>Vacant ICU Beds {selectedCenter["Vacant_ICU_beds"]}</div>
               <div>
                 Vacant Normal beds {selectedCenter["Vacant_normal_beds"]}
               </div>
-              <div>
+              <div style={{ color: "blue" }}>
                 <a href={`tel:${selectedCenter["Contact"].split(";")[0]}`}>
                   Contact {selectedCenter["Contact"]}
+                </a>
+              </div>
+              <div style={{ color: "blue" }}>
+                <a href={createNavLink(selectedCenter.lat, selectedCenter.lng)}>
+                  Directions{" "}
                 </a>
               </div>
             </div>
