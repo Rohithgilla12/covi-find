94c88b53ee4ebbdaa39b15ecf366161f0ef62248
Rohithgilla12 <gillarohith1@gmail.com>
2021-04-28 18:56:19 +0530

    PWA done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,40 +4,12 @@ import NextDocument, {
   Main,
   NextScript,
 } from "next/document";
-import Head from "next/head";
 import { ColorModeScript } from "@chakra-ui/react";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-          />
-
-          <meta name="description" content="Covi Find" />
-          <meta name="keywords" content="corona helpline" />
-          <title>Covi Find</title>
-
-          <link rel="manifest" href="/manifest.json" />
-
-          <link
-            href="/favicon-16x16.png"
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-          />
-          <link
-            href="/favicon-32x32.png"
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-          />
-        </Head>
         <ChakraHead />
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { CTA } from "../components/CTA";
 import { useState } from "react";
 import dynamic from "next/dynamic";
 import { Options, SelectedButtonContext } from "../context/ButtonSelection";
+import Head from "next/head";
 
 const MapComponentNoSSR = dynamic<any>(() => import("../components/Map"), {
   ssr: false,
@@ -14,6 +15,34 @@ const Index = () => {
 
   return (
     <Container height="100vh">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+        <meta name="description" content="Covi Find" />
+        <meta name="keywords" content="corona helpline" />
+        <title>Covi Find</title>
+
+        <link rel="manifest" href="/manifest.json" />
+
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+      </Head>
       <SelectedButtonContext.Provider value={{ selctedButton, setSelection }}>
         <Container mt={"10vh"}>
           <MapComponentNoSSR />
